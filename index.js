76b10d3ef71635329442affba306b90f76c0d2a4
faysalshuvo <faysalshuvo.dev@gmail.com>
2021-11-18 11:18:00 +0600

    Parsing Req

--- index.js
+++ index.js
@@ -7,13 +7,15 @@
 
 // dependencies
 const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
 
 // app object - module scaffolding
 const app = {};
 
 // configuration
 app.config = {
-  port: 8000,
+  port: 6000,
 };
 
 // create server
@@ -26,8 +28,27 @@ app.createServer = () => {
 
 // handle req res
 app.handleReqRes = (req, res) => {
-  // response handle
-  res.end("Hello");
+  // get the url and parse it
+  const parseUrl = url.parse(req.url, true);
+
+  const path = parseUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObj = parseUrl.query;
+  const headersObj = req.headers;
+
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+    // response handle
+    res.end("Hello");
+  });
 };
 
 // start the server
