48eb4f4590b9487aa1bfc0d12c1029ec02645e8e
faysalshuvo <faysalshuvo.dev@gmail.com>
2021-11-20 15:03:31 +0600

    environment

--- index.js
+++ index.js
@@ -8,20 +8,16 @@
 // dependencies
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
+const environment = require("./helpers/environment");
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-  port: 6000,
-};
-
 // create server
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
-  server.listen(app.config.port, () => {
-    console.log("Server RUnning......");
+  server.listen(environment.port, () => {
+    console.log(`Server at ${environment.port}......`);
   });
 };
 
